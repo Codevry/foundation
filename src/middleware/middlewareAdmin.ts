abc7bbf00829e223f8a9d6729d18c557651c187a
Saksham <dawnimpulse@gmail.com>
2025-08-11 19:04:10 +0530

    feat(middleware): admin - key change

--- src/middleware/middlewareAdmin.ts
+++ src/middleware/middlewareAdmin.ts
@@ -5,7 +5,7 @@ import Globals from "@/src/utils/globals.ts";
 export default function () {
     return bearerAuth({
         verifyToken: async (token: string, c: Context) => {
-            return token === (await Globals.dbRedis.get("/admin/key"));
+            return token === (await Globals.dbRedis.get("admin:key"));
         },
         noAuthenticationHeaderMessage: {
             success: false,
