710232abb94c1083502cbb639286aaabb3dc6964
Saksham <dawnimpulse@gmail.com>
2025-08-11 18:41:58 +0530

    feat(schema): keys - generate

---
+++ src/schema/schemaKeys.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { EnumPeriod } from "@/src/utils/enums.ts";
+
+export default {
+    generate: {
+        body: z
+            .object({
+                limit: z.number().min(1),
+                period: z.enum(EnumPeriod),
+                customPeriod: z.number().optional(),
+            })
+            .superRefine((data, ctx) => {
+                if (
+                    data.period === EnumPeriod.custom &&
+                    data.customPeriod === undefined
+                )
+                    ctx.addIssue({
+                        code: z.ZodIssueCode.custom,
+                        message:
+                            "customPeriod is required when period is set to custom",
+                        path: ["customPeriod"], // Specify the field the error is related to
+                    });
+            }),
+    },
+};
