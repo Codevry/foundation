df1ce9c5d1bd93c232568aaf824fc2948e6b2dc8
Saksham <dawnimpulse@gmail.com>
2025-08-11 19:03:36 +0530

    feat(services): admin - key path

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -5,11 +5,11 @@ export default class Admin {
      * generate a new admin key
      */
     async generateKey() {
-        const existing = await Globals.dbRedis.get("/admin/key");
+        const existing = await Globals.dbRedis.get("admin:key");
         if (existing) return existing;
         else {
-            await Globals.dbRedis.create("/admin/key", crypto.randomUUID());
-            return await Globals.dbRedis.get("/admin/key");
+            await Globals.dbRedis.create("admin:key", crypto.randomUUID());
+            return await Globals.dbRedis.get("admin:key");
         }
     }
 }
