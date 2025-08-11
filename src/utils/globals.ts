44e0cd065599fed906fab8b82dc2e09d79f0efb5
Saksham <dawnimpulse@gmail.com>
2025-08-11 19:03:45 +0530

    feat(globals): ctrlKeys

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,7 +1,9 @@
 import type { DbRedis } from "@/src/services/dbRedis.ts";
 import type Admin from "@/src/services/admin.ts";
+import CtrlKeys from "@/src/controller/ctrlKeys.ts";
 
 export default class Globals {
     static dbRedis: DbRedis;
     static admin: Admin;
+    static ctrlKeys = new CtrlKeys();
 }
