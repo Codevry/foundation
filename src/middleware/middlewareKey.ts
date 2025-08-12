6da0b81071adf69e9a5fa3bd1d6d0f952717f6a0
Saksham <dawnimpulse@gmail.com>
2025-08-12 19:08:10 +0530

    feat(middleware): key - set keyData in context

--- src/middleware/middlewareKey.ts
+++ src/middleware/middlewareKey.ts
@@ -1,11 +1,17 @@
 import { bearerAuth } from "hono/bearer-auth";
 import type { Context } from "hono";
 import Globals from "@/src/utils/globals.ts";
+import type { TypeKey } from "@/src/types/typeKey.ts";
 
 export default function () {
     return bearerAuth({
         verifyToken: async (token: string, c: Context) => {
-            return await Globals.dbRedis.exists(`api:keys:${token}`);
+            const data = await Globals.dbRedis.get(`api:keys:${token}`);
+            if (!data) return false;
+            else {
+                c.set("apiKeyData", JSON.parse(data) as TypeKey);
+                return true;
+            }
         },
         noAuthenticationHeaderMessage: {
             success: false,
