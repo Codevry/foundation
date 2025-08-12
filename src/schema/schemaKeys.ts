b186d8ccff2e0cd6037e85923543cb36e2456edc
Saksham <dawnimpulse@gmail.com>
2025-08-12 11:37:19 +0530

    feat(schema): keys - modify

--- src/schema/schemaKeys.ts
+++ src/schema/schemaKeys.ts
@@ -22,4 +22,25 @@ export default {
                     });
             }),
     },
+    modify: {
+        body: z
+            .object({
+                key: z.uuid(),
+                limit: z.number().min(1),
+                period: z.enum(EnumPeriod),
+                customPeriod: z.number().optional(),
+            })
+            .superRefine((data, ctx) => {
+                if (
+                    data.period === EnumPeriod.custom &&
+                    data.customPeriod === undefined
+                )
+                    ctx.addIssue({
+                        code: z.ZodIssueCode.custom,
+                        message:
+                            "customPeriod is required when period is set to custom",
+                        path: ["customPeriod"], // Specify the field the error is related to
+                    });
+            }),
+    },
 };
