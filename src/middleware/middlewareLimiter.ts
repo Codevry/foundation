fdf0ee9355eff8664d1ffca6d95f08ee19c4d56f
Saksham <dawnimpulse@gmail.com>
2025-08-12 19:42:04 +0530

    feat(middleware): limiter - logic revamped

--- src/middleware/middlewareLimiter.ts
+++ src/middleware/middlewareLimiter.ts
@@ -2,64 +2,85 @@ import { createMiddleware } from "hono/factory";
 import type { Context, Next } from "hono";
 import type { TypeKey } from "@/src/types/typeKey.ts";
 import Globals from "@/src/utils/globals.ts";
-import CtrlLimits from "@/src/controller/ctrlLimits.ts";
 import Time from "@/src/services/time.ts";
 import { ErrorObject } from "@/src/utils/errorObject.ts";
 
 export default createMiddleware(async (c: Context, next: Next) => {
-    /**
-     * set rate limiting headers on response
-     */
-    function setRateHeaders(remaining: number, keyData: TypeKey) {
-        c.header("X-RateLimit-Limit", keyData.limit.toString());
-        c.header("X-RateLimit-Remaining", remaining.toString());
-        c.header(
-            "X-RateLimit-Reset",
-            Time.calculateRedisTTL(
-                keyData.period,
-                keyData.customPeriod
-            ).toString()
-        );
-    }
-
-    // key data
+    // fetch key data
     const keyData: TypeKey = c.get("apiKeyData");
 
-    // form the redis key name
-    const key = `api:limits:${keyData.key}`;
-    const limitValue = await Globals.dbRedis.get(key);
-    if (limitValue) {
-        const remaining = await CtrlLimits.decLimitKey(keyData.key).catch(
-            (err) => {
-                if (err instanceof ErrorObject) {
-                    if (err.status === 429) {
-                        const resetAt = Time.calculateRedisTTL(
-                            keyData.period,
-                            keyData.customPeriod
-                        );
-                        const resetIn = resetAt - Date.now();
-                        setRateHeaders(0, keyData);
-                        c.header("X-Retry-After", resetIn.toString());
-                        c.status(429);
-                        return c.json({
-                            success: false,
-                            message: "rate limit reached",
-                            data: {
-                                limit: keyData.limit,
-                                remaining: 0,
-                                resetInSeconds: resetIn,
-                                resetAtTime: resetAt,
-                            },
-                        });
-                    }
-                } else throw err;
-            }
-        );
+    // get next window
+    const resetAt = Time.calculateRedisTTL(
+        keyData.period,
+        keyData.customPeriod
+    );
+
+    // set rate-limiting headers
+    function setRateHeaders(remaining: number) {
+        c.header("X-RateLimit-Limit", keyData.limit.toString());
+        c.header("X-RateLimit-Remaining", remaining.toString());
+        c.header("X-RateLimit-Reset", resetAt.toString());
     }
+
+    // get limit value
+    const key = `api:limits:${keyData.key}`;
+    const limit = await Globals.dbRedis.get(key);
+
+    // if value exists
+    if (limit) {
+        const limitValue = Number(limit);
+
+        // limit exceeded
+        if (limitValue === 0) {
+            // get a reset window in seconds
+            const resetIn = resetAt - Date.now();
+
+            // set rate headers
+            setRateHeaders(0);
+            c.header("X-Retry-After", resetIn.toString());
+
+            // set status and return error
+            c.status(429);
+            return c.json({
+                success: false,
+                message: "rate limit reached",
+                data: {
+                    limit: keyData.limit,
+                    remaining: 0,
+                    reset: {
+                        inSeconds: resetIn,
+                        atTime: resetAt,
+                    },
+                },
+            });
+        }
+
+        // else decrement counter, set headers and proceed
+        else {
+            // decrement counter
+            await Globals.dbRedis
+                .dec(`api:limits:${keyData.key}`)
+                .catch((err) => new ErrorObject(502, err));
+
+            // set headers & proceed
+            setRateHeaders(limitValue - 1);
+            return next();
+        }
+    }
+
     // create new limit
     else {
-        await CtrlLimits.createLimitKey(keyData);
-        setRateHeaders(keyData.limit, keyData);
+        // create a new entry in redis
+        await Globals.dbRedis
+            .create(`api:limits:${keyData.key}`, keyData.limit)
+            .catch((err: any) => new ErrorObject(502, err));
+
+        // set expiry
+        await Globals.dbRedis
+            .setTTL(`api:limits:${keyData.key}`, resetAt)
+            .catch((err) => new ErrorObject(502, err));
+
+        setRateHeaders(keyData.limit);
         return next();
     }
 });
