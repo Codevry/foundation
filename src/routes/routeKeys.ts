add71ef1c063071e202b63c1aba2203c03c68db5
Saksham <dawnimpulse@gmail.com>
2025-08-12 11:37:32 +0530

    feat(ctrl): keys - modify

--- src/routes/routeKeys.ts
+++ src/routes/routeKeys.ts
@@ -5,7 +5,7 @@
 import { type Context, Hono } from "hono";
 import middlewareAdmin from "@/src/middleware/middlewareAdmin.ts";
 import { MiddlewareSchemaValidate } from "@/src/middleware/middlewareSchema.ts";
-import schemaAdmin from "@/src/schema/schemaKeys.ts";
+import schemaKeys from "@/src/schema/schemaKeys.ts";
 import Globals from "@/src/utils/globals.ts";
 import { MiddlewareResponse } from "@/src/middleware/middlewareResponse.ts";
 
@@ -16,9 +16,18 @@ app.use(middlewareAdmin());
 // create a new api key
 app.post(
     "/",
-    MiddlewareSchemaValidate(schemaAdmin.generate),
+    MiddlewareSchemaValidate(schemaKeys.generate),
     MiddlewareResponse(async (c: Context) => {
         return Globals.ctrlKeys.generate(await c.req.json());
+    })
+);
+
+// modify the existing key
+app.put(
+    "/",
+    MiddlewareSchemaValidate(schemaKeys.modify),
+    MiddlewareResponse(async (c: Context) => {
+        return Globals.ctrlKeys.modify(await c.req.json());
     })
 );
 
