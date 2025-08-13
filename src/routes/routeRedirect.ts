a3348c43c45c1f3c16f2eeca8c64cb4495c4e735
Saksham <dawnimpulse@gmail.com>
2025-08-13 12:30:22 +0530

    fix(routes): redirect - changes to redirect path

--- src/routes/routeRedirect.ts
+++ src/routes/routeRedirect.ts
@@ -1,16 +1,33 @@
 import { Hono } from "hono";
+import middlewareKey from "@/src/middleware/middlewareKey.ts";
+import middlewareLimiter from "@/src/middleware/middlewareLimiter.ts";
 
 const app = new Hono();
 
+/**
+ * create query string from hono records
+ * @param query
+ */
+function queryString(query: Record<string, string>) {
+    const searchParams = new URLSearchParams(query);
+    return searchParams.toString();
+}
+
 // open routes
 app.all("/open/*", async (c) => {
-    const url = Bun.env.REDIRECT_OPEN + c.req.path + c.req.url.search;
+    const url =
+        Bun.env.REDIRECT_OPEN +
+        c.req.path.replace("/route/open", "") +
+        queryString(c.req.query());
     return await fetch(url, c.req.raw);
 });
 
 // secure routes
-app.all("/secure/*", async (c) => {
-    const url = Bun.env.REDIRECT_URL_SECURE + c.req.path + c.req.url.search;
+app.all("/secure/*", middlewareKey(), middlewareLimiter, async (c) => {
+    const url =
+        Bun.env.REDIRECT_URL_SECURE +
+        c.req.path.replace("/route/secure", "") +
+        queryString(c.req.query());
     return await fetch(url, c.req.raw);
 });
 
