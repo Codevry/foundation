8ae71de0d3b6268180a98b95c609ff35a29dffda
Saksham <dawnimpulse@gmail.com>
2025-08-14 14:39:18 +0530

    feat(index): middleware unhandled

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import routeKeys from "@/src/routes/routeKeys.ts";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import routeRedirect from "@/src/routes/routeRedirect.ts";
+import { MiddlewareUnhandled } from "@/src/middleware/middlewareUnhandled.ts";
 
 // initializers
 Globals.dbRedis = new DbRedis();
@@ -26,6 +27,7 @@ console.log(key);
 const app = new Hono();
 
 app.use(logger());
+app.use(MiddlewareUnhandled());
 
 app.route("/admin/keys", routeKeys);
 app.route("/route", routeRedirect);
